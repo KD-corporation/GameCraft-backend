591f6c480990f75f86e29ac5011c2a3db3f161ef
gk022135 <gk022135@gmail.com>
2025-09-14 20:02:08 +0530

    add get routes methods in routes.go file

--- routes/routes.go
+++ routes/routes.go
@@ -1,6 +1,7 @@
 package routes
 
 import (
+	"fmt"
 	"gamecraft-backend/controllers/auths"
 	"net/http"
 )
@@ -10,3 +11,18 @@ func RegisterRouter(mux *http.ServeMux) {
 	mux.HandleFunc("/login", auths.Login)
 	mux.HandleFunc("/logout", auths.Logout)
 }
+
+// only get request are Alloweed to this Function
+func RegisterRouterGet(mux *http.ServeMux) {
+	//here you can map the get requests to their respective handler functions
+
+	mux.HandleFunc("/getuser", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(`{"message": "send some data without writing any function"}`))
+	})
+
+	fmt.Println("GET /getuser route registered")
+}
+
+// similar you can do for the put and delete reuest
